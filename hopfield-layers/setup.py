cd1c97e5e264c375f689a6dee25c173fc39c3d94
Priyam Ghosh <priyamghosh9753@gmail.com>
2026-05-24 03:27:26 +0530

    chore(setup): update package metadata to DAHN fork identity (BUG-8)
    Replace upstream ml-jku metadata with correct fork identity:
      name:        hopfield-layers-dahn
      version:     0.1.0
      author:      Priyam Ghosh
      url:         https://github.com/hopfileds/hopfield-layers
      description: DAHN: Diffusion-Augmented Hopfield Networks

--- hopfield-layers/setup.py
+++ hopfield-layers/setup.py
@@ -4,12 +4,12 @@ with open(r'README.md', mode=r'r') as readme_handle:
     long_description = readme_handle.read()
 
 setuptools.setup(
-    name=r'hopfield-layers',
-    version=r'1.0.2',
-    author=r'Bernhard Schäfl',
-    author_email=r'schaefl@ml.jku.at',
-    url=r'https://github.com/ml-jku/hopfield-layers',
-    description=r'Continuous modern Hopfield layers for Deep Learning architectures',
+    name=r'hopfield-layers-dahn',
+    version=r'0.1.0',
+    author=r'Priyam Ghosh',
+    author_email=r'',
+    url=r'https://github.com/hopfileds/hopfield-layers',
+    description=r'DAHN: Diffusion-Augmented Hopfield Networks (fork of ml-jku/hopfield-layers)',
     long_description=long_description,
     long_description_content_type=r'text/markdown',
     packages=setuptools.find_packages(),
